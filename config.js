11a9fa163b45dbc157e11bfaadeb02045ab86cf0
Hoan Thien <ginothien05@gmail.com>
2021-09-16 23:43:10 +0700

    hii

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Thế giới vì tiền mà phiền não
+    Còn em vì anh mà điên đảo.`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, Em gái à :3',
+    question:'Anh biết mùa này là mùa gì không <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Mùa này là mùa thích bên anhh <33333333',
+    mess: 'Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón em nhé.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/tranthianhnguyet.info' 
+}
